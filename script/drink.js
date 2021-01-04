eb3d3740f9f3652c0abae5e95e48627eaaf5d972
gammaled <guleduni@yahoo.co.uk>
2021-01-04 16:09:47 +0000

    corrected logic of else if statement

--- script/drink.js
+++ script/drink.js
@@ -2,10 +2,10 @@ drinkAbout = function(age){
     if (age <= 0) {
     return "Sorry. I can’t tell what drink because that age is incorrect!";
     
-    } else if (age<=14) {
-        return "Drink Coke";
+    } else if (age <= 14) {
+        return "Drink Toddy";
 
     } else {
-        return "Drink Toddy";
+        return "Drink Coke";
     }
 }
