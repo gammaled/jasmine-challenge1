a6c8e9f15cb3837e5efea206e027d0a7a5b05daf
gammaled <guleduni@yahoo.co.uk>
2021-01-04 15:47:59 +0000

    added test for when age < 0 and <14

---
+++ spec/drinkSpec.js
@@ -0,0 +1,16 @@
+describe("drink test", function() {
+    
+    beforeEach(function() {
+        age = 0;
+    });
+    describe("When age is less than 0", function() { 
+        it("Should return 'Sorry. I can’t tell what drink because that age is incorrect!'", function() {
+            var result = drinkAbout(0);
+            expect(result).toBe("Sorry. I can’t tell what drink because that age is incorrect!");
+        });
+        it("Drink Toddy", function() {
+        var result = drinkAbout(13);
+        expect(result).toBe("Drink Toddy");
+    });
+    });
+}); 
