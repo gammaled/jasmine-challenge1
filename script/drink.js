ae16a1687be1cdaa144325f1830b0283cad138a1
gammaled <guleduni@yahoo.co.uk>
2021-01-04 16:01:14 +0000

    fixed the logic of script age < 0 to <= 0

--- script/drink.js
+++ script/drink.js
@@ -1,5 +1,5 @@
 drinkAbout = function(age){
-    if (age < 0) {
+    if (age <= 0) {
     return "Sorry. I can’t tell what drink because that age is incorrect!";
     } else {
         return "Drink Toddy";
