53894b8f321bb95f8db252c873f050b0d17cf29f
gammaled <guleduni@yahoo.co.uk>
2021-01-04 17:29:48 +0000

    corrected by changing else if statement to age <= 18 from 21

--- script/drink.js
+++ script/drink.js
@@ -1,11 +1,14 @@
-drinkAbout = function(age){
+drinkAbout = function(age) {
     if (age <= 0) {
     return "Sorry. I can’t tell what drink because that age is incorrect!";
     
     } else if (age <= 14) {
         return "Drink Toddy";
 
-    } else {
+    } else if (age <= 18) {
         return "Drink Coke";
+
+    } else {
+        return "Drink Beer";
     }
-}
+};
