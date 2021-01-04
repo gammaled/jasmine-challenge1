e7585f5e2b94c9939d97c88bc7e62ac7024f6689
gammaled <guleduni@yahoo.co.uk>
2021-01-04 16:08:35 +0000

    added else if statement when age <=14

--- script/drink.js
+++ script/drink.js
@@ -1,6 +1,10 @@
 drinkAbout = function(age){
     if (age <= 0) {
     return "Sorry. I can’t tell what drink because that age is incorrect!";
+    
+    } else if (age<=14) {
+        return "Drink Coke";
+
     } else {
         return "Drink Toddy";
     }
